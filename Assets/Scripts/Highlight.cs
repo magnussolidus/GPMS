04b864cc892ea172566ee5c80d565489631d9b2d
Magno Lomardo <magnussolidus@gmail.com>
2021-02-05 04:13:54 -0300

    created repo

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Highlight : MonoBehaviour
+{
+    public Color highlightColor;
+    
+    private MeshRenderer _myRender;
+    private Color _defaultColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _defaultColor = this.gameObject.GetComponent<MeshRenderer>().material.color;
+
+        if(_myRender== null)
+        {
+            _myRender = this._myRender = this.gameObject.GetComponent<MeshRenderer>();
+        }
+        
+    }
+
+    private void OnMouseEnter()
+    {
+        _myRender.material.color = highlightColor;
+        Debug.Log("Entrou");
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("Saiu");
+        _myRender.material.color = _defaultColor;
+    }
+
+}
